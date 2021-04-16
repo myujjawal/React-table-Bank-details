4f9e4d66e15bbc8f9d9c7eda6e361d2343ec4775
myujjawal <myujjawal@gmail.com>
2021-04-17 02:18:37 +0530

    API issue solved

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import React, {useState,useEffect,useMemo} from "react";
 import TableStructure from "./components/TableStructure"
+import './components/table.css'
 
 require("es6-promise").polyfill();
 require("isomorphic-fetch")
 
+
+
 export default function App() {
     const initial = JSON.parse(window.localStorage.getItem('data')) || []; //Getting data cache on clients localStorage
     const [data,setData]=useState(initial);
+    
+    const [city,setCity]=useState("MUMBAI");
 
     
     //Cached API Call
     useEffect(()=>{
-        fetch("https://vast-shore-74260.herokuapp.com/banks?city=MUMBAI")
+        // city = setCity(JSON.parse(window.localStorage.getItem('city'))|| "MUMBAI")
+        fetch("https://vast-shore-74260.herokuapp.com/banks?city="+city)
         .then((response)=>response.json())
         .then((json)=>{
             setData(json); //Setting the data to state
             window.localStorage.setItem('data',JSON.stringify(json)) //Creating data cache on clients localStorage
+            window.localStorage.setItem('city',JSON.stringify(city))
         })
+
     }
-    ,[]);
+    ,[city]);
 
     const COLUMN = useMemo(
         () => [{
@@ -50,7 +58,22 @@ export default function App() {
     
 
 
-    return(
+    return(<>
+        <div className='top'>
+        <h1>Bank Searches</h1>
+        <label for="city">Choose a City: </label>
+
+        <select 
+        onChange={(e) => setCity(e.target.value)}
+        name="city" id="city">
+        <option value="MUMBAI">Mumbai</option>
+        <option value="BANGALORE">Bangalore</option>
+        <option value="DELHI">Delhi</option>
+        <option value="PUNE">Pune</option>
+        <option value="AGARTALA">Agartala</option>
+        </select>
+        </div>
+
         <TableStructure columns={COLUMN} data={data} />
-    )
+    </>)
 }
--- src/components/GlobalSearch.js
+++ src/components/GlobalSearch.js
@@ -4,18 +4,7 @@ import './table.css'
 export const GlobalSearch = ({filter,setFilter})=>{
     return (<>
     <div className='Head'>
-    <h3>Bank Searches</h3>
-        <label for="city">Choose a City:</label>
-
-        <select name="city" id="city">
-        <option value="Mumbai">Mumbai</option>
-        <option value="Bangalore">Bangalore</option>
-        <option value="Delhi">Delhi</option>
-        <option value="Patna">Patna</option>
-        <option value="Agartala">Agartala</option>
-        </select>
-
-        <span className='Top'>Search:{''}
+        <span className='Top'>Search: {''}
         <input value={filter||''} onChange={(e)=>setFilter(e.target.value)}  />
         </span>
         </div>
